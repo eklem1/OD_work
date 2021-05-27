579fe9cc15b160c49cca4aee5d7a7d434527a4d8
eklem1 <emma.klemets@mail.mcgill.ca>
2021-05-27 12:15:39 -0400

    added function to formate wavelength dep data; got Daya bay transmission data

---
+++ LittleFunction.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+"""
+Rename STL files the fusion saves to removed unneeded parts.
+"""
+def renameSTLs(path):
+    print("Looking in " + path)
+    
+    for filename in os.listdir(path):
+        if filename.endswith(".stl"): 
+    #         print(filename)
+            x = filename.split('_')
+
+            if "WT" in filename:
+                old = filename
+                new = x[0] + "_" + x[5] + "_" + x[-1]
+
+    #             print(r"{}{}".format(path, new))
+            else:
+                old = filename
+                new = x[0] + "_" + x[2] + x[3]
+
+    #             print(r"{}{}".format(path, new))
+
+            print("Rename {} to {}".format(old, new))
+            os.rename(r"{}{}".format(path, old),r"{}{}".format(path, new))
+            continue
+
+        else:
+            continue
+            
+"""
+Prints out wavelenght dependant values for the Yaml file in nice format for given wavelength
+bounds.
+bounds = [lower, upper] limits of wavelength that should be printed
+"""
+def ListForYaml(wavelength, values, bounds=None):
+
+    if isinstance(bounds, list):
+        wavelength = wavelength[(wavelength > bounds[0]) & (bounds[1]>wavelength)]
+
+    for i, wl in enumerate(wavelength):
+            
+        print("{}: !!python/tuple [{:.5},{:.5}]".format(i, wl, values[i]))
